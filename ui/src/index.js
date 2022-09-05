33b8b42488e09f5dce624962fd266aa44c09a4fe
Johanna Wirell <jw223gx@student.lnu.se>
2022-09-05 17:26:08 +0200

    Delete previous content

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,12 @@
 import { DSU } from './DSU.js' 
 
 function printElements(arr) {
+  // Delete previous content
   const treediv = document.querySelector('.tree')
-
+  while(treediv.firstChild){
+    treediv.removeChild(div.firstChild);
+  }
+  
   for (const element of arr) {
     const newDiv = document.createElement("div");
     const newP = document.createElement("p")
@@ -17,7 +21,11 @@ function printElements(arr) {
 }
 
 function printParents(arr) {
+  // Delete previous content
   const parentdiv = document.querySelector('.parents')
+  while(parentdiv.firstChild){
+    parentdiv.removeChild(div.firstChild);
+  }
 
   for (const parent of arr) {
     console.log(parent)
@@ -40,6 +48,9 @@ function main() {
 
   console.log("Elements: " + elements)
   console.log("Parents: " + parents)
+
+  printElements(elements)
+  printParents(parents)
 
   printElements(elements)
   printParents(parents)
