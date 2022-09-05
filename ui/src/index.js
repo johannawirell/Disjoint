f255430ae83e66aecdeabe3c23da4ad0e5bc95fd
Johanna Wirell <jw223gx@student.lnu.se>
2022-09-05 17:31:09 +0200

    Connect index and parents

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,23 +1,25 @@
 import { DSU } from './DSU.js' 
 
-function printElements(arr) {
+function printElements(arr, parents) {
   // Delete previous content
   const treediv = document.querySelector('.tree')
   while(treediv.firstChild){
     treediv.removeChild(treediv.firstChild);
   }
   
-  for (const element of arr) {
+  for (let i = 0; i < arr.length; i++) {
     const newDiv = document.createElement("div");
     const newP = document.createElement("p")
-    const newContent = document.createTextNode(element)
+    const newContent = document.createTextNode(arr[i])
     newDiv.classList.add('element')
-    newDiv.id = element
+    newDiv.id = parents[i]
 
     newP.appendChild(newContent);
     newDiv.appendChild(newP);
     treediv.appendChild(newDiv)
   }
+
+  printParents(parents)
 }
 
 function printParents(arr) {
@@ -28,7 +30,6 @@ function printParents(arr) {
   }
 
   for (const parent of arr) {
-    console.log(parent)
     const newDiv = document.createElement("div");
     const newP = document.createElement("p")
     const newContent = document.createTextNode(parent)
@@ -46,13 +47,10 @@ function main() {
   const elements = dsu.getElementIndexes();
   const parents = dsu.getParents();
 
-  printElements(elements)
-  printParents(parents)
+  printElements(elements, parents)
 
   dsu.unionByHeight(0, 1)
 
-  printElements(elements)
-  printParents(parents)
 
 }
 
